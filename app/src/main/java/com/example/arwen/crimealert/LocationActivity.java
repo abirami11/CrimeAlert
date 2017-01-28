e5da339725c3d0cc6efb657acb23694ce0bd450d
abirami11 <rm.abirami@gmail.com>
2017-01-27 17:26:47 -0800

    Update LocationActivity.java

--- app/src/main/java/com/example/arwen/crimealert/LocationActivity.java
+++ app/src/main/java/com/example/arwen/crimealert/LocationActivity.java
@@ -10,13 +10,15 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class LocationActivity extends AppCompatActivity {
-
+    TextView txtLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        txtLocation = (TextView) findViewById(R.id.incidents);
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
@@ -32,13 +34,24 @@ public class LocationActivity extends AppCompatActivity {
         };
         int permissionCheck = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
+        txtLocation.setText("Latitude:");
         if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            Log.d("Network", "Network");
+            if (locationManager != null) {
+                Location loc = locationManager
+                        .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                txtLocation.setText("Latitude:" + loc.getLatitude() + ", Longitude:" + loc.getLongitude());
+
+            }
         }
 
     }
 
     private void newLocationChange(Location location) {
+
+        txtLocation = (TextView) findViewById(R.id.incidents);
+        txtLocation.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
         Log.d("STATE", "New Location Change");
     }
 
